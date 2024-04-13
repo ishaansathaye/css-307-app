389460780ea02d0e476adb6b7958056e152b627f
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-04-12 19:18:48 -0700

    added form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
 import Table from "./Table";
-
+import Form from "./Form";
 
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor"
-        },
-        {
-          name: "Mac",
-          job: "Bouncer"
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress"
-        },
-        {
-          name: "Dennis",
-          job: "Bartender"
-        }
-      ]);
+    const [characters, setCharacters] = useState([]);
 
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -36,6 +19,7 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
         />
+        <Form />
       </div>
     );
 }
