9c6ae7451758cf69c78318ddd378d33156bb01aa
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-04-12 19:23:02 -0700

    finished ie1

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState({
     name: "",
     job: ""
@@ -11,6 +11,11 @@ function Form() {
     if (name === "job")
       setPerson({ name: person["name"], job: value });
     else setPerson({ name: value, job: person["job"] });
+  }
+
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" });
   }
 
   return (
@@ -31,6 +36,7 @@ function Form() {
         value={person.job}
         onChange={handleChange}
       />
+        <input type="button" value="Submit" onClick={submitForm} />
     </form>
   );
   
