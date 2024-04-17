17ed0f803715205c5a962c79e4c1afbbdaca2837
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-04-17 09:41:50 -0700

    step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,15 +35,18 @@ const users = {
     ]
 };
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
 const findUserByName = (name) => {
     return users["users_list"].filter(
         (user) => user["name"] === name
     );
 };
+
+const findUserById = (id) =>
+users["users_list"].find((user) => user["id"] === id);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.get("/users", (req, res) => {
     const name = req.query.name;
@@ -53,6 +56,16 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
     }
 });
 
