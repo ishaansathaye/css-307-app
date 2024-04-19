c2b9320d7cbedc5af4d492a05ed51416a36f4708
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2024-04-19 10:54:56 -0700

    step 3 of ie3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,7 +5,7 @@ import Form from "./Form";
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
-
+    
     useEffect(() => {
         fetchUsers()
         .then((res) => res.json())
@@ -20,8 +20,12 @@ function MyApp() {
         setCharacters(updated);
     }
     
-    function updateList(person) {
-        setCharacters([...characters, person]);
+    function updateList(person) { 
+        postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+            console.log(error);
+        })
     }
     
     function fetchUsers() {
@@ -29,15 +33,27 @@ function MyApp() {
         return promise;
     }
     
-    return (
-        <div className="container">
-        <Table 
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-        />
-        <Form handleSubmit={updateList}/>
-        </div>
-    );
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+    });
+    
+    return promise;
+}
+
+return (
+    <div className="container">
+    <Table 
+    characterData={characters}
+    removeCharacter={removeOneCharacter}
+    />
+    <Form handleSubmit={updateList}/>
+    </div>
+);
 }
 
 export default MyApp;
